2d6489354462b5df6772a4faa8291436955a4be1
Rowan Delander <rowan@x13.localdomain>
2025-12-21 15:52:49 -0800

    Added csv-parser to TungstenBuild

--- TungstenRuntime/src/main.cpp
+++ TungstenRuntime/src/main.cpp
@@ -3,12 +3,12 @@
 
 // Forward declorations
 #define W_COMPONENT_X(Type, Name, Include) class Type;
-#include TUNGSTEN_PROJECT_COMPONENT_LIST_INCLUDE_PATH
+#include TUNGSTEN_PROJECT_COMPONENT_LIST_INCLUDE
 #undef W_COMPONENT_X
 
 // Includes
 #define W_COMPONENT_X(Type, Name, Include) #include Include
-#include TUNGSTEN_PROJECT_COMPONENT_LIST_INCLUDE_PATH
+#include TUNGSTEN_PROJECT_COMPONENT_LIST_INCLUDE
 #undef W_COMPONENT_X
 
 int main()
